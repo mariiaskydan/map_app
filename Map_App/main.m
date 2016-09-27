88645751c772ae7479c24bb20a988edf2ab28c58
Mariya Dychko <mdychko@icloud.com>
2016-09-27 11:15:35 +0300

    fix commit

---
+++ Map_App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Map_App
+//
+//  Created by m dychko on 9/25/16.
+//  Copyright © 2016 m dychko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target ‘Map_App’ do
+	pod 'GoogleMaps'
+end
